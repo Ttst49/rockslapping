43e264e9449a594cbd8b46a4e4634aa23f5ece5a
esdb1 <pierre.lacognata@mail-esd.com>
2022-10-21 12:45:23 +0200

    more stuff again

--- js/main.js
+++ js/main.js
@@ -6,8 +6,30 @@ const timeLeft = document.querySelector('#timeLeft')
 let position;
 
 function randomlySpawnChris(){
+//enlever toutes les classes chrisRock => plus de chris nulle part
+    cells.forEach(cell=>{cell.classList.remove('chrisRock')})
+//une cellule au hasard recoit la classe chrisRock
+    let randomCell = cells[Math.floor(Math.random()*9)]
+    randomCell.classList.add('chrisRock')
+
+    position = randomCell.id
+
+    //on a l'id de la cellule en train d'afficher
+    // chris stockée dans la variable position
+
+    //on peut surveiller le clic sur chaque cellule
+
+    //sur le clic d'une cellule, l'ID de cette derniere doit etre égal
+    //à la variable position
+    //si c'est le cas, on ajoute un point au score
+
+
 
 }
+randomlySpawnChris()
+
+setInterval(randomlySpawnChris,400)
+
 
 // definir un chiffre au hasard entre 0 et 8 (inclus)
 //Math.floor(Math.random()*9)
