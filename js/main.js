f70ea9e241d2fa3b440aeb9c308c56bd3f7dd237
esdb1 <pierre.lacognata@mail-esd.com>
2022-10-21 12:22:02 +0200

    more stuff

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const cells = document.querySelectorAll('.cell')
+const chris = document.querySelector('.chrisRock')
+const score = document.querySelector('#score')
+const timeLeft = document.querySelector('#timeLeft')
+
+let position;
+
+function randomlySpawnChris(){
+
+}
+
+// definir un chiffre au hasard entre 0 et 8 (inclus)
+//Math.floor(Math.random()*9)
+// cells[1].classList.add('chrisRock')
+
+
